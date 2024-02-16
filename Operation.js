f604797c20f50575b395a7d13b6233f52dc732a7
Mohammed Faisal <mohammedelormf@gmail.com>
2024-02-16 22:32:45 +0000

    Working on basic operations

---
+++ Operation.js
@@ -0,0 +1,25 @@
+
+// Basic Addition
+function basicAddition (num1, num2) {
+  return num1 + num2;
+}
+const addition = basicAddition(20, 10);
+console.log(addition);
+// Basic Subtraction
+function basicSubtraction (num1, num2) {
+  return num1 - num2;
+}
+const subtraction = basicSubtraction(20, 10);
+console.log(subtraction);
+// Basic Multiplication
+function basicMultiplication (num1, num2) {
+  return num1 * num2;
+}
+const multiplication = basicMultiplication(20, 10);
+console.log(multiplication);
+// Basic Division
+function basicDivision (num1, num2) {
+  return num1 / num2;
+}
+const division = basicDivision(20, 10);
+console.log(division);
